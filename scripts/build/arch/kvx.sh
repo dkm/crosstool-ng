0c868c1a5f98d6fb532107b0b2bf6fd7bd4c6a6b
Marc Poulhiès <marc.poulhies@kalray.eu>
2019-11-08 14:34:45 +0100

    Simple initial support for KVX MPPA
    Adds the strict minimum to build a bare-metal toolchain for KVX MPPA
    by providing custom git path to Kalray's github repositories.

---
+++ scripts/build/arch/kvx.sh
@@ -0,0 +1,13 @@
+# Compute KVX-specific values
+
+CT_DoArchTupleValues() {
+    CT_TARGET_ARCH="${CT_ARCH}"
+}
+
+
+# CT_DoArchUClibcConfig()
+# {
+#     local cfg="${1}"
+
+#     CT_DoArchUClibcSelectArch "${cfg}" "kvx"
+# }
